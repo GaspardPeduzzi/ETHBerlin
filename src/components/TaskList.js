83e0acbec2675744c2e3f0530c4c3a7818d48775
jean <jean.chambras@epfl.ch>
2018-09-08 15:31:49 +0200

    Styling TaskItems and TaskList

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -3,39 +3,47 @@ import React from 'react'
 import TaskItem from "./TaskItem"
 class TaskList extends React.Component {
 
-    constructor(){
+    constructor() {
         super();
         this.state = {
-            tasksId: [1,2,3]
+            tasksId: [1, 2, 3]
         }
         this.createList = this.createList.bind(this);
     }
 
-createList(){
-    return (this.state.tasksId.map((id) =>
-        <TaskItem id = {id} name="" description="" key={id.toString()}/>
-));
-}
-
-  render(){
-    return (
-        <div className="container is-narrow">
-
-            <div className="task-list">
-                    {this.createList()}
+    createList() {
+        return (this.state.tasksId.map((id) =>
+            <div className="column">
+                <TaskItem id={id} name="" description="" key={id.toString()} />
             </div>
-            <div className="columns is-centered">
-                <div className="column has-text-centered">
-                    <a className="button is-primary">Primary</a>
+
+        ));
+    }
+
+    render() {
+        return (
+
+
+            <section className="section has-background-light">
+                <div className="container is-narrow">
+                    <div className="box">
+                        <a href=""> 
+                        <div className="column">
+                            {this.createList()}
+                        </div>
+                        <div className="columns is-centered">
+                            <div className="column has-text-centered">
+                                <a className="button is-primary is-large is-rounded">+</a>
+                            </div>
+                        </div>
+                        
+                        </a>
+                        
+                    </div>
                 </div>
-            </div>
-            
-        </div>
-        
-
-    );
-    
-}
+            </section>
+        );
+    }
 }
 
 export default TaskList;
