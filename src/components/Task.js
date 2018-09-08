70535d4ee2e88470fdf09247cce4e17c48030f34
Louis Merlin <louis.merlin@epfl.ch>
2018-09-08 21:02:07 +0200

    Fix task review

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,16 +1,14 @@
 import React from 'react'
 
 class Task extends React.Component {
-
-    
   render() {
     return (
-            <div className="content">
-                <h2 className="task-title is-size-1 has-text-weight-bold has-text-grey-darker">Task Title</h2>
-                <p className="tast-details has-text-weight-light has-text-grey">
-                {this.props.task} 
-                </p>        
-            </div>
+      <div className="content">
+        <h2 className="task-title is-size-1 has-text-weight-bold has-text-grey-darker">{this.props.task.title}</h2>
+        <p className="tast-details has-text-weight-light has-text-grey">
+          {this.props.task.description}
+        </p>
+      </div>
     );
   }
 }
--- src/components/TaskDetails.js
+++ src/components/TaskDetails.js
@@ -11,13 +11,12 @@ class TaskDetails extends React.Component {
   async componentDidMount() {
     const taskId = this.props.match.params.taskId;
     const client = this.props.colonyClient;
-    const task = await client.getTask.call({ taskId: Number(taskId)})
-    console.log('hello')
+    const task = await client.getTask.call({ taskId: Number(taskId) + 1})
     try {
       const result = (await this.props.node.files.cat(`/ipfs/${task.specificationHash}`)).toString();
-      this.setState({ task: await result });
+      this.setState({ task: JSON.parse(await result) });
     } catch(e) {
-      this.setState({ task: await 'Could not load task' });
+      this.setState({ task: await {title: 'Could not load task', description: ''}});
     }
   }
 
@@ -30,7 +29,7 @@ class TaskDetails extends React.Component {
             <div className="columns is-centered">
               <div className="column has-text-centered">
                 <a className="button is-submit-blue is-rounded is-large">
-                     SUBMIT
+                  SUBMIT
                 </a>
               </div>
             </div>
