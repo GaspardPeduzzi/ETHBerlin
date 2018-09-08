2c0782848033faa80f555b411528aed019b79167
Louis Merlin <louis.merlin@epfl.ch>
2018-09-08 15:26:36 +0200

    Fix lint issues

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,41 +1,44 @@
 import React from 'react'
-
 import TaskItem from "./TaskItem"
+
 class TaskList extends React.Component {
-
-    constructor(){
-        super();
-        this.state = {
-            tasksId: [1,2,3]
-        }
-        this.createList = this.createList.bind(this);
+  constructor() {
+    super();
+    this.state = {
+      tasksId: [1, 2, 3]
     }
+    this.createList = this.createList.bind(this);
+  }
 
-createList(){
+  createList() {
     return (this.state.tasksId.map((id) =>
-        <TaskItem id = {id} name="" description="" key={id.toString()}/>
-));
-}
+      <TaskItem
+        id={id}
+        name = ""
+        description = ""
+        key = {
+          id.toString()
+        }
+      />
+    ));
+  }
 
-  render(){
+  render() {
     return (
-        <div className="container is-narrow">
-
-            <div className="task-list">
-                    {this.createList()}
-            </div>
-            <div className="columns is-centered">
-                <div className="column has-text-centered">
-                    <a className="button is-primary">Primary</a>
-                </div>
-            </div>
-            
+      <div className = "container is-narrow">
+        <div className = "task-list" >
+          {this.createList()}
         </div>
-        
-
+        <div className="columns is-centered">
+          <div className="column has-text-centered">
+            <a className="button is-primary">
+              Primary
+            </a>
+          </div>
+        </div>
+      </div>
     );
-    
-}
+  }
 }
 
 export default TaskList;
